e593f321bee2c7bc5bf874df53d6167164c19838
abdullahbest123 <abdullahbest19@gmail.com>
2023-03-28 20:36:10 +0500

    Updates

---
+++ precache-manifest.c8a1560afed688624e3a5f77e7497331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98f2e0ea70b0198aade0d3ccca39a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05186339f9b4819146bf",
+    "url": "/static/css/main.2e2bf056.chunk.css"
+  },
+  {
+    "revision": "659926fc3d1b4da11abf",
+    "url": "/static/js/2.4914f7da.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4914f7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05186339f9b4819146bf",
+    "url": "/static/js/main.3ea00e9a.chunk.js"
+  },
+  {
+    "revision": "35d8ea8e0de0fd66811a",
+    "url": "/static/js/runtime-main.bb80f66d.js"
+  }
+]);
